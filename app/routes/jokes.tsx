1f90818bbf04e2ddf523f5e5e2b591c72962242d
ilovehackathons <106395723+ilovehackathons@users.noreply.github.com>
2022-11-06 20:41:26 +0100

    look at the docs and adjust the jokes code
    Mostly just a matter of reordering the code.

--- app/routes/jokes.tsx
+++ app/routes/jokes.tsx
@@ -1,28 +1,27 @@
-import type { LinksFunction } from "@remix-run/node";
-import { Outlet, Link } from "@remix-run/react";
-import type { LoaderFunction } from "@remix-run/node";
-import { json } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import type { LinksFunction, LoaderFunction } from "@remix-run/node";
 import type { Joke } from "@prisma/client";
+import { json } from "@remix-run/node";
+import { Link, Outlet, useLoaderData } from "@remix-run/react";
 
-import stylesUrl from "~/styles/jokes.css";
 import { db } from "~/utils/db.server";
+import stylesUrl from "~/styles/jokes.css";
 
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: stylesUrl }];
 };
 
-type LoaderData = { jokes: Array<Joke> };
+type LoaderData = { jokeListItems: Array<Joke> };
 
 export const loader: LoaderFunction = async () => {
   const data: LoaderData = {
-    jokes: await db.joke.findMany({ take: 5 }),
+    jokeListItems: await db.joke.findMany(),
   };
   return json(data);
 };
 
 export default function JokesRoute() {
   const data = useLoaderData<LoaderData>();
+
   return (
     <div className="jokes-layout">
       <header className="jokes-header">
@@ -41,7 +40,7 @@ export default function JokesRoute() {
             <Link to=".">Get a random joke</Link>
             <p>Here are a few more jokes to check out:</p>
             <ul>
-              {data.jokes.map((joke) => (
+              {data.jokeListItems.map((joke) => (
                 <li key={joke.id}>
                   <Link to={joke.id}>{joke.name}</Link>
                 </li>
