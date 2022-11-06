c121dcfa7d164e9301f08e57803af6e3fd71ff27
ilovehackathons <106395723+ilovehackathons@users.noreply.github.com>
2022-11-06 18:30:11 +0100

    add the links exports to the root and jokes routes
    Checked the CSS files to figure out the media query widths.

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,28 @@
 import { Links, LiveReload, Outlet } from "@remix-run/react";
+import type { LinksFunction } from "@remix-run/node";
+
+import stylesUrl from "~/styles/global.css";
+import largeStylesUrl from "~/styles/global-large.css";
+import mediumStylesUrl from "~/styles/global-medium.css";
+
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: "stylesheet",
+      href: stylesUrl,
+    },
+    {
+      rel: "stylesheet",
+      href: largeStylesUrl,
+      media: "(min-width: 1024px)",
+    },
+    {
+      rel: "stylesheet",
+      href: mediumStylesUrl,
+      media: "(min-width: 640px)",
+    },
+  ];
+};
 
 export default function App() {
   return (
