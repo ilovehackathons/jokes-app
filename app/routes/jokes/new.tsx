f75389da6baad077b641417939f9704f65bd2129
ilovehackathons <106395723+ilovehackathons@users.noreply.github.com>
2022-11-07 11:27:47 +0100

    add a joke to the DB on form submission
    Checked the linked docs and previous code to implement this.

--- app/routes/jokes/new.tsx
+++ app/routes/jokes/new.tsx
@@ -1,3 +1,19 @@
+import type { ActionFunction } from "@remix-run/node";
+import { redirect } from "@remix-run/node";
+import { db } from "~/utils/db.server";
+
+export const action: ActionFunction = async ({ request }) => {
+  const body = await request.formData();
+  const name = body.get("name");
+  const content = body.get("content");
+  if (!(typeof name === "string" && typeof content === "string"))
+    throw new Error("Name or content not found");
+  const joke = await db.joke.create({
+    data: { name, content },
+  });
+  return redirect(`/jokes/${joke.id}`);
+};
+
 export default function NewJokeRoute() {
   return (
     <div>
