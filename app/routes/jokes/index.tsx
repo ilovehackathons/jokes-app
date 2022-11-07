6ce82c7e0f612e78adf5ba50d46bd8f3dc6cf98e
ilovehackathons <106395723+ilovehackathons@users.noreply.github.com>
2022-11-07 10:42:05 +0100

    look at the docs and correct the code
    There was a redundant parameter left over from the copied-over code.

--- app/routes/jokes/index.tsx
+++ app/routes/jokes/index.tsx
@@ -1,19 +1,19 @@
-import type { Joke } from "@prisma/client";
 import type { LoaderFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
-import { Link, useLoaderData } from "@remix-run/react";
+import { useLoaderData, Link } from "@remix-run/react";
+import type { Joke } from "@prisma/client";
+
 import { db } from "~/utils/db.server";
 
 type LoaderData = { randomJoke: Joke };
 
-export const loader: LoaderFunction = async ({ params }) => {
+export const loader: LoaderFunction = async () => {
   const count = await db.joke.count();
   const randomRowNumber = Math.floor(Math.random() * count);
   const [randomJoke] = await db.joke.findMany({
     take: 1,
     skip: randomRowNumber,
   });
-
   const data: LoaderData = { randomJoke };
   return json(data);
 };
