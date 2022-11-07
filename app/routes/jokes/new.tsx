5fea47cd18df5a8e12f0356d7d32dd3279650bf1
ilovehackathons <106395723+ilovehackathons@users.noreply.github.com>
2022-11-07 12:04:43 +0100

    check the solution and correct the code
    Type in the corrections by hand.

--- app/routes/jokes/new.tsx
+++ app/routes/jokes/new.tsx
@@ -1,16 +1,21 @@
 import type { ActionFunction } from "@remix-run/node";
 import { redirect } from "@remix-run/node";
+
 import { db } from "~/utils/db.server";
 
 export const action: ActionFunction = async ({ request }) => {
-  const body = await request.formData();
-  const name = body.get("name");
-  const content = body.get("content");
-  if (!(typeof name === "string" && typeof content === "string"))
-    throw new Error("Name or content not found");
-  const joke = await db.joke.create({
-    data: { name, content },
-  });
+  const form = await request.formData();
+  const name = form.get("name");
+  const content = form.get("content");
+  // we do this type check to be extra sure and to make TypeScript happy
+  // we'll explore validation next!
+  if (typeof name !== "string" || typeof content !== "string") {
+    throw new Error("Form not submitted correctly.");
+  }
+
+  const fields = { name, content };
+
+  const joke = await db.joke.create({ data: fields });
   return redirect(`/jokes/${joke.id}`);
 };
 
