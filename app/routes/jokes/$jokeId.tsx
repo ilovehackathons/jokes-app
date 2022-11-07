5518480f707d9bd6c421d6953e53178559c7290e
ilovehackathons <106395723+ilovehackathons@users.noreply.github.com>
2022-11-07 07:31:08 +0100

    get a specific joke from the DB
    Copied over the code from the jokes route and modified it accordingly.

--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,10 +1,33 @@
+import type { Joke } from "@prisma/client";
+import type { LoaderFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { Link } from "react-router-dom";
+
+import { db } from "~/utils/db.server";
+
+type LoaderData = {
+  joke: Joke;
+};
+
+export const loader: LoaderFunction = async ({ params }) => {
+  const data: LoaderData = {
+    joke: await db.joke.findUniqueOrThrow({
+      where: { id: params.jokeId },
+    }),
+  };
+  return json(data);
+};
+
 export default function JokeRoute() {
+  const data = useLoaderData<LoaderData>();
+
   return (
     <div>
       <p>Here's your hilarious joke:</p>
+      <p>{data.joke.content}</p>
       <p>
-        Why don't you find hippopotamuses hiding in trees? They're really good
-        at it.
+        <Link to={data.joke.id}>{data.joke.name} Permalink</Link>
       </p>
     </div>
   );
